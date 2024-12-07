73dccef68de5c1674cea1405e8420fc7c2187c45
Gizem <gizem.ates@hvl.no>
2024-12-07 18:18:07 +0100

    tower selection bar

--- hvl_protect/panel_tower/grid.py
+++ hvl_protect/panel_tower/grid.py
@@ -12,6 +12,25 @@ class Grid:
         self.num_rows = 5
         self.num_columns = 10
         self.enemies = []
+        self.tower_selection_box_offset = [50, 400]
+        self.tower_selection_box_size = 70
+        self.space_between_tower_selection = 100
+        self.tower_selection_box_color = "black"
+        self.n_of_towers = 4
+        self.towers= 4*[pygame.Rect(0,0,0,0)]
+    
+    def generater_towers(self):
+        tower_index = 0
+        print("here")
+        for tower in self.towers:
+            tower[0] = self.tower_selection_box_offset[0] + tower_index*self.space_between_tower_selection
+            tower[1] = self.tower_selection_box_offset[1]
+            tower[2] = self.tower_selection_box_size
+            tower[3] = self.tower_selection_box_size
+            tower_index += 1
+            print(tower[0], tower[1], tower[2], tower[3])
+            pygame.draw.rect(self.screen, "black", tower)
+        
 
     def get_pixel_position(self, row, column):
         return (self.base[0] + self.width/self.num_columns/2 + column*self.width/self.num_columns, self.base[1] + self.height/self.num_rows/2 + row*self.height/self.num_rows)
@@ -45,6 +64,8 @@ class Grid:
         pygame.draw.rect(self.screen, self.color, pygame.Rect(self.base[0], self.base[1],self.width, self.height))
         self.draw_grid()    
         self.draw_enemies()    
+        self.draw_grid()        
+        self.generater_towers()
 
         
    
