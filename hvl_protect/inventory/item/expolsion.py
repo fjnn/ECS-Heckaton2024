0fe1f4c8f12e1b7ab35d4d31543f31eb41686a7b
LauEls <laurenz.elstner@gmail.com>
2024-12-08 13:04:45 +0100

    comment removed

--- hvl_protect/inventory/item/expolsion.py
+++ hvl_protect/inventory/item/expolsion.py
@@ -26,22 +26,3 @@ class Explosion:
             return None
         
         return self.imgs[self.img_index]
-
-    #     # self.swap_index()
-
-    # def swap_index(self):
-    #     """
-    #     Swap the image index to change the enemy's appearance.
-    #     """
-    #     self.swap_cntr += 1
-    #     if self.swap_cntr >= 30/self.speed:
-    #         self.img_index = 1 - self.img_index
-    #         self.swap_cntr = 0
-
-    # def move(self):
-    #     """
-    #     Move the enemy towards the base with its speed.
-    #     Movement is horizontal only.
-    #     """
-
-    #     self.position = (self.position[0] - self.speed, self.position[1])
